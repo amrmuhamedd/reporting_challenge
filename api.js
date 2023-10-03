422467ed7068c7e5654ae32b769369080d803cf5
Amr Mohamed <amrmuhmedd@gmail.com>
2023-10-03 14:14:38 +0300

    feat: Get student by id

--- api.js
+++ api.js
@@ -18,7 +18,21 @@ async function getHealth (req, res, next) {
 }
 
 async function getStudent (req, res, next) {
-  throw new Error('This method has not been implemented yet.')
+ try {
+  const {id} = req.params
+
+  if(!id || id === ':id') return res.status(400).json({message : "Valid id is required"})
+
+  const student = await knex('students').where({id}).first();
+
+  if (!student) {
+    return res.status(404).json({message : "student not found"})
+  }
+
+  res.status(200).json(student)
+ } catch (error) {
+  
+ }
 }
 
 async function getStudentGradesReport (req, res, next) {
