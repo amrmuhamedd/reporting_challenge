d24d7a7236c40f9ef2a20206f3aff491a1864dba
Amr Mohamed <amrmuhmedd@gmail.com>
2023-10-03 20:39:53 +0300

    refactor: create static file for grads

--- workers/gradeDataFetcher.js
+++ workers/gradeDataFetcher.js
@@ -1,6 +1,6 @@
 const axios = require("axios");
+const fs = require("fs");
 const { parentPort } = require("worker_threads");
-
 const gradeDataUrl =
   "https://outlier-coding-test-data.onrender.com/grades.json";
 
@@ -9,10 +9,20 @@ function fetchGradesData() {
     .get(gradeDataUrl)
     .then(({ data }) => {
       parentPort.postMessage(data);
+      const jsonData = JSON.stringify(data , null , 2)
+      const filePath = "data/staticData/gradeData.json"; 
+      
+      fs.writeFile(filePath, jsonData, (err) => {
+        if (err) {
+          console.error("Error writing JSON file:", err);
+        } else {
+          console.log("JSON file has been created!");
+        }
+      });
     })
     .catch((err) => {
       parentPort.postMessage({ error: err.message });
     });
 }
 
-fetchGradesData()
+fetchGradesData();
